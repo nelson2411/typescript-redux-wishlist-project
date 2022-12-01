8f9983962ab79e10bedc6387811b737d0ee3d886
Nelson Rosales <nelson.rosales83@gmail.com>
2022-12-01 14:39:37 +0100

    react-router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import GlobalStyle from './components/global-styles/GlobalStyle'
-import Home from './pages/home/Home'
-import './App.css'
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Products from './pages/products/Products';
+import GlobalStyle from './components/global-styles/GlobalStyle';
+import Home from './pages/home/Home';
+import './App.css';
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <Home />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/products' component={Products} />
+      </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
