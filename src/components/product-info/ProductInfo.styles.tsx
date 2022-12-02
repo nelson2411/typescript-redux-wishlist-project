45b1f9c38826dbae7e4d85dac1e7a2893ec6df34
Nelson Rosales <nelson.rosales83@gmail.com>
2022-12-02 13:04:39 +0100

    feat: solo page videogame

--- src/components/product-info/ProductInfo.styles.tsx
+++ src/components/product-info/ProductInfo.styles.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { ListGroup, Image, Badge, Col } from 'react-bootstrap';
 
 export const ProductInfoContainer = styled.div`
   min-height: 80vh;
@@ -7,4 +8,38 @@ export const ProductInfoContainer = styled.div`
   justify-content: center;
   flex-direction: row;
   align-items: center;
+`;
+
+export const InfoTextContainer = styled(Col)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ImageContainer = styled(Image)`
+  border-radius: 7px;
+  box-shadow: 12px 12px 2px 1px black;
+  width: 50%;
+`;
+
+export const InfoContainer = styled(ListGroup)`
+  background-color: transparent;
+  box-shadow: 12px 12px 2px 1px black;
+`;
+
+export const InfoGameContainer = styled(ListGroup.Item)`
+  background-color: #c68c0f;
+`;
+
+export const PlatformsContainer = styled(ListGroup.Item)`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  background-color: #c68c0f;
+`;
+
+export const BadgeContainer = styled(Badge)`
+  margin: 5px 7px;
+  padding: 8px 16px;
 `;
--- src/components/product-info/ProductInfo.tsx
+++ src/components/product-info/ProductInfo.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { VideoGame } from '../../types/videoGamesTypes';
 import { useProduct } from '../../hooks/useProduct';
-import { ProductInfoContainer } from './ProductInfo.styles';
-import { Container, Row, Col, Image, ListGroup } from 'react-bootstrap';
+import {
+  ProductInfoContainer,
+  PlatformsContainer,
+  ImageContainer,
+  InfoContainer,
+  InfoGameContainer,
+  BadgeContainer,
+  InfoTextContainer,
+} from './ProductInfo.styles';
+import { Container, Row, Col, Image, Card, ListGroup, Badge } from 'react-bootstrap';
 
 type ProductInfoProps = {
-  id: any;
+  id: number;
 };
 
 const ProductInfo = ({ id }: ProductInfoProps) => {
@@ -17,16 +25,27 @@ const ProductInfo = ({ id }: ProductInfoProps) => {
       <Container>
         <Row>
           <Col>
-            <Image src={product?.image} alt={product?.name} fluid />
+            <ImageContainer src={product?.image} alt={product?.name} fluid />
           </Col>
-          <Col>
-            <ListGroup variant='flush'>
-              <ListGroup.Item>Name: {product?.name}</ListGroup.Item>
-              <ListGroup.Item>Genre: {product?.genre}</ListGroup.Item>
-              <ListGroup.Item>Publisher: {product?.publisher}</ListGroup.Item>
-              <ListGroup.Item>Year: {product?.year}</ListGroup.Item>
-            </ListGroup>
-          </Col>
+          <InfoTextContainer>
+            <InfoContainer>
+              <InfoGameContainer>Name: {product?.name}</InfoGameContainer>
+              <InfoGameContainer>Genre: {product?.genre}</InfoGameContainer>
+              <InfoGameContainer>Publisher: {product?.publisher}</InfoGameContainer>
+              <InfoGameContainer>Year: {product?.year}</InfoGameContainer>
+              <InfoGameContainer>Price: {product?.price}</InfoGameContainer>
+              <PlatformsContainer>
+                Platforms:
+                {product?.platforms.map((platform, index) => (
+                  <ListGroup horizontal key={index}>
+                    <BadgeContainer bg='success'>
+                      <h6>{platform}</h6>
+                    </BadgeContainer>
+                  </ListGroup>
+                ))}
+              </PlatformsContainer>
+            </InfoContainer>
+          </InfoTextContainer>
         </Row>
       </Container>
     </ProductInfoContainer>
