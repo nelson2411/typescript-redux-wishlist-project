3edf2400a003ce203c89b285ff3d35f1e989471a
Nelson Rosales <nelson.rosales83@gmail.com>
2022-12-07 10:43:31 +0100

    feat: json-server script

---
+++ server.ts
@@ -0,0 +1,28 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+
+const jsonServer = require('json-server');
+const clone = require('clone');
+const data = require('./data/db.json');
+
+const isProductionEnv = process.env.NODE_ENV === 'production';
+const server = jsonServer.create();
+
+const router = jsonServer.router(isProductionEnv ? clone(data) : '/data/db.json', {
+  _isFake: isProductionEnv,
+});
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use((req, res, next) => {
+  if (req.path !== '/') router.db.setState(clone(data));
+  next();
+});
+
+server.use(router);
+server.listen(process.env.PORT || 8000, () => {
+  console.log('JSON Server is running');
+});
+
+module.exports = server;
